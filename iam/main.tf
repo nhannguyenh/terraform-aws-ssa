2fb7ce64292bfb55482925cfd69316e382dfbf97
Nhan Nguyen Hoang <nhannguyenhoang.hcmus@gmail.com>
2024-09-10 23:14:23 +0700

    Init terraform with AWS credentials

---
+++ iam/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  shared_config_files      = ["/Users/nhannguyenh/Documents/myws/terraform/.aws/config"]
+  shared_credentials_files = ["/Users/nhannguyenh/Documents/myws/terraform/.aws/credentials"]
+  region                   = "ap-southeast-1"
+}

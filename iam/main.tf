9f1ba8639bf1feb74f740d1dc6933404ffc9a1b3
Nhan Nguyen Hoang <nhannguyenhoang.hcmus@gmail.com>
2024-09-13 00:26:36 +0700

    Create a new user and add it to developers group

--- iam/main.tf
+++ iam/main.tf
@@ -24,3 +24,15 @@ resource "aws_iam_group_policy_attachment" "iam_read_only_access" {
   group      = aws_iam_group.developers.name
   policy_arn = "arn:aws:iam::aws:policy/IAMReadOnlyAccess"
 }
+
+# Create the news developer user
+resource "aws_iam_user" "dev1" {
+  name = "dev1"
+}
+
+# Add dev1 to the developers group
+resource "aws_iam_group_membership" "developers_group_membership" {
+  name  = "developers_group_membership"
+  group = aws_iam_group.developers.name
+  users = [aws_iam_user.dev1.name]
+}
